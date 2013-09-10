42aebea7d81136f2a768e562047556e8d03a80d2
Luciana Aoki <luk.aoki@hotmail.com>
2013-09-09 21:26:32 -0300

    Prog calculo IMC

---
+++ Imc2/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Imc2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.txtaltura = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.Location = new System.Drawing.Point(119, 50);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(100, 20);
+            this.txtpeso.TabIndex = 1;
+            // 
+            // txtaltura
+            // 
+            this.txtaltura.Location = new System.Drawing.Point(119, 87);
+            this.txtaltura.Name = "txtaltura";
+            this.txtaltura.Size = new System.Drawing.Size(100, 20);
+            this.txtaltura.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(12, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cálculo do IMC: entre com os dados:";
+            this.toolTip1.SetToolTip(this.label2, resources.GetString("label2.ToolTip"));
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.Location = new System.Drawing.Point(251, 326);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 38);
+            this.button3.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.button3, "Sair\r\n");
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(66, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Peso:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(66, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Altura:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(33, 195);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(240, 96);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(30, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Resultado:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(69, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "CALCULAR IMC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkRed;
+            this.button2.Location = new System.Drawing.Point(123, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "NOVO CÁLCULO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.ForeColor = System.Drawing.Color.Red;
+            this.lblresultado.Location = new System.Drawing.Point(125, 172);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(0, 20);
+            this.lblresultado.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 369);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtaltura);
+            this.Controls.Add(this.txtpeso);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Imc: Índice de Massa Corpórea";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.TextBox txtaltura;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ Imc2/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Imc2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string resultados()
+        {
+            if (imc <= 18.5)
+            {
+                textBox3.Text = "BAIXO PESO! \n\n Nesse ponto, o corpo magro deixa de ser saudáveal e o organismo fica mais vulnerável" +
+                    " a infeccções. Se o peso persistir, mesmo com a alimentação normal, procure ajuda médica; a dificuldade para engordar" +
+                    " pode ser sintoma de alguma doença insidiosa, como o diabetes";
+            }
+            if (imc > 18.5 && imc <= 24.9)
+            {
+                
+                textBox3.Text = "PESO IDEAL! \n\n Seu peso está adequado a sua altura. é importante manter a educação alimentar e a atividade" +
+                    "e a atividade física. fique atento caso seu valor esteja perto de limites para os estágios dde Baixo Peso ou Sobrepeso.";
+            }
+            if (imc > 24.9 && imc <= 29.9)
+            {
+                textBox3.Text = "SOBREPESO! \n\n Esta faixa indica que você está com predisposição à obesidade;" +
+                    "dependendo do seu histórico familiar e pessoal, pode haver um quadro de pré-diabetes e hipertensão" +
+                    "Procure orientação médica para evitar o próximo estágio, Obesidade 1. Quanto mais quilos extras, maior a dificuldade para emagrecer.";
+            }
+            if (imc > 29.9 && imc <= 34.9)
+            {
+                textBox3.Text = "OBESIDADE DE GRAU 1! \n\n O risco de desenvolver diabetes, para quem está nesta faixa de peso é de 20%," +
+                    "e o de hipertensão ultrapassa 25%. Há risco maior de outras doenças cardiovasculares, articulares, distúrbios psiquiátricos, apnéia do sono e até certos tipos de câncer.";
+            }
+            if (imc > 34.9 && imc <= 39.9)
+            {
+                textBox3.Text = "OBESIDADE DE SEGUNDO GRAU!! \n\n O risco de desenvolver diabetes chega a 40%, e a chance de surgirem doenças associadas a obesidade" +
+                    "com reumatismos, câncer, apneia do sono, hipertensão e outros problemas cardiovasculares pode chegar a 50%. PROCURE ORIENTAÇÃO MÉDICA!!!";
+
+            }
+            if (imc > 39.9 && imc <= 40)
+            {
+                textBox3.Text = "OBESIDADE GRAU 3\n\n O risco de desenvolver doenças associadas ao excesso de peso, como diabete, reumatismos, câncer," +
+                    "apneia do sono, hipertensão e outros problemas cardiovasculares chega a 90%. Nesse estágio, a cirurgia de redução de estômago pode ser indicada.\n\nPROCURE ORIENTAÇÃO MÉDICA IMEDIATAMENTE!!!";
+            }
+            return textBox3.Text;
+        }
+
+
+        double imc;
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //verifica valores e interage com o usuário
+            if (txtpeso.Text == "" || txtaltura.Text == "")
+            {
+                MessageBox.Show("Insira os valores para calcular!");
+            }
+            else
+            {
+                try
+                {
+                    //calculo
+                    double peso = double.Parse(txtpeso.Text), altura = double.Parse(txtaltura.Text);
+                    imc = peso / (altura * altura);
+                    lblresultado.Text = Convert.ToString(imc);
+                    resultados();
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Digite somente números e utilize apenas vírgulas!");
+                }
+            }
+        }
+
+       
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sim ou Não", MessageBoxButtons.YesNoCancel) == DialogResult.Yes)
+
+                Close();
+
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            txtaltura.Clear();
+            lblresultado.Text = null;
+            txtpeso.Clear();
+            textBox3.Clear();
+        }
+
+        
+
+        
+    }
+}
